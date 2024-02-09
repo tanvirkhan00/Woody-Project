77738cc465e43b5cd88ae0ee811fdb8ecd7c9c9f
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-02-09 23:15:10 +0600

    first commit

---
+++ src/js/swiper.js
@@ -0,0 +1,40 @@
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  }
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+    rewind: true,
+    
+  },
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }
+});
