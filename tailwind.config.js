76f41f8d6ccb8c6cdf8e07f7b403095d58cc9ec9
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-09-11 01:17:43 +0600

    correction project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+    },
+    extend: {
+      colors: {
+        tillColor:'#008080',
+        primaryColorLight:'#010d78',
+        yellowColor:'#DFFF00',
+        paragraphColor:'#c0c0c0',
+        whiteColor:'#fff',
+        blackColor:'#000',
+        greenColor:'#007936',
+        redColor:'#cc3433',
+        darkColor:'#000',
+        darkColorLight:'#171717',
+        limeColor:'#00FF00'
+      },
+    },
+    container:{
+      center:true,
+      padding: {
+        DEFAULT:'1rem',
+        sm: '1.5rem',
+      }
+    },
+    fontFamily:{
+      fontSuse: ["SUSE", 'sans-serif'],
+      fontProtest: ["Protest Guerrilla", 'sans-serif'],
+      fontLobster: ["Lobster", 'sans-serif'],
+      fontBebas: ["Bebas Neue", 'sans-serif']
+    }
   },
   plugins: [],
 }
